6c4ad0d0628e5fa61268db2f3618ff9c63bb3d75
Yukino Song <nutosservice@gmail.com>
2024-08-08 05:47:29 +0800

    Better start widecar script

--- start-widecar.ps1
+++ start-widecar.ps1
@@ -1,26 +1,33 @@
 param (
     [string]$exePath = "C:\Program Files\Sunshine\sunshine.exe",
     [string]$workingDirectory = "C:\Program Files\Sunshine\",
+    [string]$sessionId = (Get-Process -PID $pid).SessionID,
+    # Path to PsExec
+    # You can download it from https://learn.microsoft.com/en-us/sysinternals/downloads/psexec
+    [string]$psexecPath = "D:\Tools\pstools\PsExec.exe",
     # Adjust with the actual config files you want to start.
     # e.g. if you have only one widecar config, leave only the first entry.
-    [string[]]$exeParams = @(".\config\sunshine_widecar.conf", ".\config\sunshine_widecar_2.conf", ".\config\sunshine_widecar_3.conf")
+    [string[]]$exeParams = @(
+        ".\config\sunshine_widecar.conf"
+        # ,".\config\sunshine_widecar_2.conf"
+        # ,".\config\sunshine_widecar_3.conf"
+    )
 )
 
-# Path to PsExec
-# You can download it from https://learn.microsoft.com/en-us/sysinternals/downloads/psexec
-$psexecPath = "D:\Tools\pstools\PsExec.exe"
+# Self elevate the script
+# Taken from https://github.com/itsmikethetech/Virtual-Display-Driver/blob/master/Community%20Scripts/toggle-VDD.ps1
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine -WindowStyle Hidden
+        Exit
+    }
+}
 
 # Generate commands dynamically
 $commands = $exeParams | ForEach-Object {
     "powershell -Command `"start '$exePath' '$_' -WorkingDirectory '$workingDirectory' -WindowStyle Hidden`""
 }
-
-# Get the first non-zero session ID
-$users = query user
-$sessionId = $users | Select-String -Pattern "\s\d+\s" | ForEach-Object {
-    $temp = $_.Matches.Value.Trim()
-    if ($temp -ne '0') { return $temp }
-} | Select-Object -First 1
 
 # Execute each command
 foreach ($command in $commands) {
